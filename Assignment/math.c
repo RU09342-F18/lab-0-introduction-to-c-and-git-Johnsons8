444af457a71ff137ec2fb379f909ddbd22686850
Johnsons8 <35497793+Johnsons8@users.noreply.github.com>
2018-09-10 16:25:39 -0400

    Implementation
    Added all needed input and Outputs

--- Assignment/math.c
+++ Assignment/math.c
@@ -1,1 +1,56 @@
+#include 'math.h'
 
+int math(int num1, int num2, char Operator){ // inputs num1, num2 as numbers to do math with, and Operator a character of which that is used to do math functions
+if(num1 != NULL && num2 != NULL && Operator != NULL)
+{  
+ if(Operator == '+')
+ {
+   return num1 + num2;
+ }
+ elseif (Operator == '-')
+ {
+   return num1 - num2;
+ }
+ elseif (Operator == '*')
+ {
+   return num1 * num2;
+ }
+ elseif (Operator == '/')
+ {
+   return num1 / num2;
+ }
+ elseif (Operator == '%')
+ {
+   return num1 % num2;
+ }
+ elseif (Operator == '<')
+ {
+   return num1 << num2;
+ }
+ elseif (Operator == '>')
+ {   
+   return num1 >> num2;
+ }
+ elseif (Operator == '&')
+ {   
+   return num1 & num2;
+ }
+ elseif (Operator == '|')
+ {
+   return num1 | num2;
+ }
+ elseif (Operator == '^')
+ {
+   return num1 ^ num2;
+ }
+ elseif (Operator == '~')
+ {   
+   return ~num1;
+ }  
+ else
+ {
+    return -1;
+ }
+}
+
+  
